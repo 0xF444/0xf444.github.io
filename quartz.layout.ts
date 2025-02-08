45021801614c0a53e8cbf3112dc1dedbde03ee43
Omar Mokhtar <omar.shatat95@gmail.com>
2025-02-08 07:32:17 +0200

    Quartz sync: Feb 8, 2025, 7:32 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -34,7 +34,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.DesktopOnly(Component.TableOfContents())
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.DesktopOnly(Component.Graph())
   ],
 }
 
