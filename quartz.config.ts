a8e8ccf9c0e5a9fbff4420c9992a8f5dac4bdd28
Omar Mokhtar <omar.shatat95@gmail.com>
2025-08-17 01:57:33 +0300

    Quartz sync: Aug 17, 2025, 1:57 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://0xf444.github.io",
+    baseUrl: "0xf444.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
